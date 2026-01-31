3e4db5d458893905811f002aecfcc67fc0867563
TaghikhaniAlireza <76070954+TaghikhaniAlireza@users.noreply.github.com>
2026-01-31 13:51:44 +0330

    Create ansibleInitalization.ps1

---
+++ vagrant-devops-lab/provisioning/ansible/ansibleInitalization.ps1
@@ -0,0 +1,21 @@
+# ansible path
+cd provisioning\ansible
+
+# making directories
+mkdir inventory
+mkdir playbooks
+mkdir roles
+mkdir roles\system_hardening
+mkdir roles\system_hardening\tasks
+mkdir roles\system_hardening\handlers
+mkdir roles\system_hardening\defaults
+
+# making files
+New-Item ansible.cfg -ItemType File
+New-Item inventory\hosts.ini -ItemType File
+New-Item playbooks\site.yml -ItemType File
+New-Item roles\system_hardening\tasks\main.yml -ItemType File
+New-Item roles\system_hardening\handlers\main.yml -ItemType File
+New-Item roles\system_hardening\defaults\main.yml -ItemType File
+
+Write-Host "✅ Ansible skeleton created successfully"
